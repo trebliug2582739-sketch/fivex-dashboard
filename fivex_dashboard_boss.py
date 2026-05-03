d9ee931379a431df16ee5a98c0a8af749b8eb545
trebliug2582739-sketch <trebliug2582739@gmail.com>
2026-05-04 00:57:22 +0800

    Add files via upload

--- fivex_dashboard_boss.py
+++ fivex_dashboard_boss.py
@@ -108,13 +108,21 @@ def get_gc():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    try:
-        # Streamlit Cloud - read from secrets
-        import json
-        creds_dict = dict(st.secrets["gcp_service_account"])
+    if "gcp_service_account" in st.secrets:
+        creds_dict = {
+            "type": st.secrets["gcp_service_account"]["type"],
+            "project_id": st.secrets["gcp_service_account"]["project_id"],
+            "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+            "private_key": st.secrets["gcp_service_account"]["private_key"],
+            "client_email": st.secrets["gcp_service_account"]["client_email"],
+            "client_id": st.secrets["gcp_service_account"]["client_id"],
+            "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+            "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+        }
         creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
-    except:
-        # Local - read from file
+    else:
         creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
     return gspread.authorize(creds)
 
